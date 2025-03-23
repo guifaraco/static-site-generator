7099bfff69b7ec1a70b11de550705d5c1a43fbba
Guilherme Vieira <guirfv29@gmail.com>
2025-03-22 22:39:37 -0300

    feat: leafnode

---
+++ src/test_leafnode.py
@@ -0,0 +1,19 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_init(self):
+        node = LeafNode(tag="div", value="Hello World")
+        self.assertEqual(node.tag, "div")
+        self.assertEqual(node.value, "Hello World")
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.props, None)
+
+    def test_to_html(self):
+        node = LeafNode(tag="div", value="Hello World")
+        self.assertEqual(node.to_html(), "<div>Hello World</div>")
+        node = LeafNode(tag="div", value=None)
+        self.assertRaises(ValueError, node.to_html)
+
+if __name__ == '__main__':
+    unittest.main()
