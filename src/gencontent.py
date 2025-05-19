0f59e6e4533c286210e7b3e20146093c6b4bd742
Guilherme Vieira <guirfv29@gmail.com>
2025-05-18 23:11:05 -0300

    feat: changing the `public` directory to `docs`, so I can put in GitHub Pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,17 +2,17 @@ import os
 from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path, "r")
     template_file = open(template_path, "r")
@@ -27,6 +27,8 @@ def generate_page(from_path, template_path, dest_path):
 
         full_html = content_template.replace("{{ Title }}", title)
         full_html = full_html.replace("{{ Content }}", html)
+        full_html = full_html.replace('href="/', 'href="' + basepath)
+        full_html = full_html.replace('src="/', 'src="' + basepath)
 
         dest_dir_path = os.path.dirname(dest_path)
         if dest_dir_path != "":
